95f572397b0b1d9f7ef41c1183c5b94a404e946d
Manas George <manas.george@gatech.edu>
2014-12-28 16:08:44 +0400

    Added test for symmetry

--- src/UtilitiesTest.java
+++ src/UtilitiesTest.java
@@ -7,5 +7,12 @@ public class UtilitiesTest {
         for (int i = 1; i < 50; i++){
             assertEquals(i + "C1 should be" + i,i,Utilities.binomial(i,1));
         }
+
+        for (int i = 1; i < 50; i++){
+            for(int j = 1; j < i / 2; j++){
+                assertEquals(i + "C" + j + " should equal "
+                + i + "C" + (i-j),Utilities.binomial(i,j), Utilities.binomial(i,i-j));
+            }
+        }
     }
 }
